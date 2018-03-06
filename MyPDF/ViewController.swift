9410f81ae4cb9b46fe15d7108e845fc612a56d31
KoingDev <sea.saingkoing@gmail.com>
2018-03-06 22:04:59 +0700

    Add Split Array for Swift 4

--- MyPDF/ViewController.swift
+++ MyPDF/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
 
 }
 
-// FIXME: Should replace with Swift 4
+// FIXME: - Should replace with Swift 4
 extension Array {
     func chunks(_ chunkSize: Int) -> [[Element]] {
         return stride(from: 0, to: self.count, by: chunkSize).map {
@@ -37,3 +37,23 @@ extension Array {
         }
     }
 }
+
+// MARK: - Swift 4 Edition
+//let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+//
+//extension Array {
+//    func splitBy(subSize: Int) -> [[Element]] {
+//        return 0.stride(to: self.count, by: subSize).map { startIndex in
+//            let endIndex = startIndex.advancedBy(subSize, limit: self.count)
+//            return Array(self[startIndex ..< endIndex])
+//        }
+//    }
+//}
+//
+//let chunks = arr.splitBy(5)
+//
+//print(chunks) // [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12]]
+
+
+
+
